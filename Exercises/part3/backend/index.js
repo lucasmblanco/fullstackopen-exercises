7fe8391f59560f77b88f0b07573a102447c411a6
Lucas Matias Blanco <lucasmatiasblanco@outlook.com>
2025-02-24 12:41:27 -0300

    Repository updated

--- Exercises/part3/backend/index.js
+++ Exercises/part3/backend/index.js
@@ -1,36 +1,38 @@
-const express = require('express'); 
-const morgan = require('morgan'); 
-const app = express(); 
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
 
 let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
     },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
     },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
     },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
     }
 ]
 
 
 app.use(express.static('build'))
+morgan.token('body', (req) => JSON.stringify(req.body));
 app.use(express.json());
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body')); 
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 app.get('/api/persons', (req, res) => {
-    res.json(persons); 
+    res.json(persons);
 })
 
 app.get('/info', (req, res) => {
@@ -38,49 +40,49 @@ app.get('/info', (req, res) => {
     <p>Phonebook has info for ${persons.length} people</p>
     <p>${new Date()}</p>
     `
-    res.send(response); 
+    res.send(response);
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
-    const chosen = persons.find(person => person.id === Number(req.params.id)); 
+    const chosen = persons.find(person => person.id === Number(req.params.id));
     if (!chosen) {
         next();
     }
-    res.json(chosen); 
-    
+    res.json(chosen);
+
 })
 
 app.post('/api/persons', (req, res, next) => {
     if (!req.body.number) {
-        return res.status(404).json({ error: 'Missing number data' }); 
+        return res.status(404).json({ error: 'Missing number data' });
     }
 
-    const nameAlreadyIncluded = persons.filter(person => person.name === req.body.name); 
+    const nameAlreadyIncluded = persons.filter(person => person.name === req.body.name);
 
     if (nameAlreadyIncluded.length > 0) {
-        return res.status(404).json({ error: 'name must be unique' }); 
+        return res.status(404).json({ error: 'name must be unique' });
     }
 
-    const newPerson = {}; 
-    newPerson.id = Math.random(); 
-    newPerson.name = req.body.name; 
+    const newPerson = {};
+    newPerson.id = Math.random();
+    newPerson.name = req.body.name;
     newPerson.number = req.body.number;
 
-    persons.push(newPerson); 
+    persons.push(newPerson);
     morgan.token('body', req => JSON.stringify(req.body))
-    
+
     res.json(newPerson);
-    
+
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
-    const newArr = persons.filter(person => person.id !== Number(req.params.id)); 
-    persons = newArr; 
-    res.json(persons); 
+    const newArr = persons.filter(person => person.id !== Number(req.params.id));
+    persons = newArr;
+    res.json(persons);
 })
 
 app.use((req, res) => {
-    res.status(404).json({error: 'not found, sorry'}); 
+    res.status(404).json({ error: 'not found, sorry' });
 })
 
 
